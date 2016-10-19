1b89806ae85baa84d21a887f4233c251ca488985
Claudio <claudiocruz2002@hotmail.com>
2016-10-18 22:02:48 -0200

    Implementado Strings resource.

--- app/src/main/java/com/example/rm30966/olimpiadas/HomeActivity.java
+++ app/src/main/java/com/example/rm30966/olimpiadas/HomeActivity.java
@@ -16,6 +16,7 @@ import android.widget.Spinner;
 import android.widget.ArrayAdapter;
 import android.widget.SpinnerAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -24,7 +25,7 @@ public class HomeActivity extends AppCompatActivity{
     private Spinner spEsportes;
     private RadioGroup rgHorarios;
     private EditText edtNome, edtIdade;
-    private TextView edtNomeD, edtIdadeD,edtModalidadeD, edtHorarioD;
+    private TextView txtNomeD, txtIdadeD,txtModalidadeD, txtHorarioD;
     private AlertDialog.Builder builder;
     private Dialog confirmar;
 
@@ -48,32 +49,40 @@ public class HomeActivity extends AppCompatActivity{
 
     public void reservar(View v)
     {
-        confirmar = new Dialog(this);
-        confirmar.setContentView(R.layout.dialog);
+        if(edtNome.getText().toString().equals("") || edtIdade.getText().toString().equals(""))
+        {
+            Toast tst = Toast.makeText(this,R.string.msgErroHome,Toast.LENGTH_LONG);
+            edtNome.requestFocus();
+        }
+        else
+        {
+            confirmar = new Dialog(this);
+            confirmar.setContentView(R.layout.dialog);
 
-        edtNomeD = (TextView) confirmar.findViewById(R.id.edtNomeD);
-        edtIdadeD = (TextView) confirmar.findViewById(R.id.edtIdadeD);
-        edtHorarioD = (TextView) confirmar.findViewById(R.id.edtHorarioD);
-        edtModalidadeD = (TextView) confirmar.findViewById(R.id.edtModalidadeD);
+            txtNomeD = (TextView) confirmar.findViewById(R.id.edtNomeD);
+            txtIdadeD = (TextView) confirmar.findViewById(R.id.edtIdadeD);
+            txtHorarioD = (TextView) confirmar.findViewById(R.id.edtHorarioD);
+            txtModalidadeD = (TextView) confirmar.findViewById(R.id.edtModalidadeD);
 
-        edtNomeD.setText(edtNome.getText());
-        edtIdadeD.setText(edtIdade.getText());
+            txtNomeD.setText(edtNome.getText());
+            txtIdadeD.setText(edtIdade.getText());
 
-        int idSelecionado = rgHorarios.getCheckedRadioButtonId();
-        RadioButton rbSelecionado = (RadioButton) findViewById(idSelecionado);
-        edtHorarioD.setText(rbSelecionado.getText());
+            int idSelecionado = rgHorarios.getCheckedRadioButtonId();
+            RadioButton rbSelecionado = (RadioButton) findViewById(idSelecionado);
+            txtHorarioD.setText(rbSelecionado.getText());
 
-        edtModalidadeD.setText(spEsportes.getSelectedItem().toString());
+            txtModalidadeD.setText(spEsportes.getSelectedItem().toString());
 
-        confirmar.show();
+            confirmar.show();
+        }
     }
 
     public void confirmaD(View v)
     {
         builder = new AlertDialog.Builder(this);
-        builder.setTitle("Confirmação");
+        builder.setTitle(R.string.lbConfirmarD);
         builder.setCancelable(true);
-        builder.setNeutralButton("OK",  new DialogInterface.OnClickListener() {
+        builder.setNeutralButton(R.string.lbOKD,  new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 confirmar.dismiss();
